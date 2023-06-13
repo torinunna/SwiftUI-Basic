4c0428ac314e443c5e7c82dcebae0f9efe311347
torinunna <torinunna9588@gmail.com>
2023-06-13 23:56:47 +0900

    Initial Commit

---
+++ SwiftUI-Basic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Basic
+//
+//  Created by YUJIN KWON on 2023/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI-Basic
+//
+//  Created by YUJIN KWON on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
