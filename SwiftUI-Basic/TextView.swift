4d3b8427e90ab7f808eb0b8a2adb36c70610d8d2
torinunna <torinunna9588@gmail.com>
2023-06-14 23:50:04 +0900

    create TextView

---
+++ SwiftUI-Basic/TextView.swift
@@ -0,0 +1,20 @@
+//
+//  TextView.swift
+//  SwiftUI-Basic
+//
+//  Created by YUJIN KWON on 2023/06/14.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text("SwiftUI")
+    }
+}
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextView()
+    }
+}
