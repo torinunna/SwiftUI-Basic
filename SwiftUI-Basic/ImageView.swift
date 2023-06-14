de04fe227e5df4f4891b14619e313b2566db7624
torinunna <torinunna9588@gmail.com>
2023-06-14 23:51:58 +0900

    create ImageView

---
+++ SwiftUI-Basic/ImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ImageView.swift
+//  SwiftUI-Basic
+//
+//  Created by YUJIN KWON on 2023/06/14.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Image(systemName: "sun.max.fill")
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView()
+    }
+}
