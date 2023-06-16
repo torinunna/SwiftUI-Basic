f8df4c730b7219e4a4f05dacf1b12a326652d5e2
torinunna <torinunna9588@gmail.com>
2023-06-16 11:55:58 +0900

    add spacer

--- SwiftUI-Basic/TextView.swift
+++ SwiftUI-Basic/TextView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct TextView: View {
     var body: some View {
-        Text("SwiftUI")
-            .font(.system(size: 40.0, weight: .bold, design: .default))
+        
+        HStack {
+            Text("SwiftUI")
+                .font(.system(size: 40.0, weight: .bold, design: .default))
+            Spacer()
+        }
+        
     }
 }
 
